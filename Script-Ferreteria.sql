ccc1d6e65c68f66967b20399096cbe6dece9c4aa
jairellgit <133938234+jairellgit@users.noreply.github.com>
2025-11-02 17:34:08 -0600

    Add description and estado fields to producto table

--- Script-Ferreteria.sql
+++ Script-Ferreteria.sql
@@ -9,15 +9,20 @@ CREATE TABLE categoria(
 CREATE TABLE producto (
   id_producto INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(100),
+  descripcion VARCHAR(300),
   precio DECIMAL(5,2),
+  existencias INT,
   id_categoria INT,
   FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria),
-  imagen_ruta TEXT
+  imagen_ruta TEXT,
+  estado boolean
 );
 
 CREATE USER 'usuario_prueba'@'localhost' IDENTIFIED BY 'Usuar1o_Clave.';
 GRANT ALL PRIVILEGES ON ferreteria.* TO 'usuario_prueba'@'localhost';
-FLUSH PRIVILEGES;
+
+INSERT INTO categoria (nombre) VALUES
+('Herramientas');
 
 COMMIT;
 
