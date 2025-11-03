5f73252f2c06569d6f9902d820abda5b4256d67a
jairellgit <133938234+jairellgit@users.noreply.github.com>
2025-11-02 18:35:56 -0600

    Add NOT NULL constraints to producto table fields

--- Script-Ferreteria.sql
+++ Script-Ferreteria.sql
@@ -8,14 +8,14 @@ CREATE TABLE categoria(
 
 CREATE TABLE producto (
   id_producto INT AUTO_INCREMENT PRIMARY KEY,
-  nombre VARCHAR(100),
+  nombre VARCHAR(100) NOT NULL,
   descripcion VARCHAR(300),
-  precio DECIMAL(5,2),
-  existencias INT,
-  id_categoria INT,
+  precio DECIMAL(5,2) NOT NULL,
+  existencias INT NOT NULL,
+  id_categoria INT NOT NULL,
   FOREIGN KEY (id_categoria) REFERENCES Categoria(id_categoria),
   imagen_ruta TEXT,
-  estado boolean
+  estado boolean NOT NULL
 );
 
 CREATE USER 'usuario_prueba'@'localhost' IDENTIFIED BY 'Usuar1o_Clave.';
